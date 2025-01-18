ec0c8de30eda3dded9170cd53ca7cae3a09b7cd3
Md.Shoharab chowdhury <156209351+shoharab122@users.noreply.github.com>
2025-01-18 22:39:14 +0600

    Add files via upload

---
+++ python5.py
@@ -0,0 +1,28 @@
+
+
+print("Addition(+)")
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+result = num1 + num2
+print(f"The result of {num1} + {num2} is {result}.")
+#use of string 
+
+a="harry"
+print(a[0])
+print(a[1])
+print(a[2])
+print(a[3])
+print(a[4])
+
+print ("lets use for loop \n")
+#use for loop
+for character in a:
+    print(character)
+#Strings Slicing and Operations on Strings in Python
+print("length of a string\n" )
+fruit="Mango"
+MangoLen=len(fruit)
+print(MangoLen)
+print(fruit[0:5])
+print(fruit[0:-3])
+print(fruit[-3:-1])
