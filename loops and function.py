55b0182f58caad90e74d45a17ae1510f364e41e7
Md.Shoharab chowdhury <156209351+shoharab122@users.noreply.github.com>
2025-04-18 22:52:36 +0600

    Add files via upload

---
+++ loops and function.py
@@ -0,0 +1,9 @@
+
+
+#average of numbers
+
+number2 = int(input("Enter the number:"))
+number3= int(input("Enter the number:"))
+def average(number1, number2):
+    return (number1 + number2) / 2  # Example logic for average
+print(f"The average is: {average(number2, number3)}")
